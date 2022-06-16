e909ee41c9f69f569a3e7574c3c4df022841c15e
huynhduvan <72264118+huynhduvan@users.noreply.github.com>
2022-06-17 00:52:49 +0700

    Updated

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,53 +1,70 @@
-//using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
-//namespace WebApplication1.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    public class WeatherForecastController : ControllerBase
-//    {
-//        private static readonly string[] Summaries = new[]
-//        {
-//            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//        };
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-//        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
-//        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-//        {
-//            _logger = logger;
-//        }
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        
+        private static List<WeatherForecast> weatherForecast = new List<WeatherForecast>();
 
-//        private static List<WeatherForecast> weatherForecast = new List<WeatherForecast>();
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult GetAll()
+        {
+            var db = new WeatherForecastContext();
+            return Ok(db.WeatherForecasts);
+        }
+        //    var lst = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    }).ToList();
+        //    _context.WeatherForecasts.Add(lst);
+        //    return lst;
+        //    // var filteredList = lst.Where(forecast => forecast.TemperatureC >= 10 && forecast.TemperatureC <= 20);
+        //    // var filteredList = new List<WeatherForecast>();
+        //    // foreach (var forecast in filteredList)
+        //    // {
+        //    //     if (forecast.TemperatureC >= 10 && forecast.TemperatureC <= 20)
+        //    //     {
+        //    //         filteredList.Add(forecast);
+        //    //     }
+        //    // }
+        //    // return filteredList;
 
-//        [HttpGet(Name = "GetWeatherForecast")]
-//        public IEnumerable<WeatherForecast> Get()
-//        {
-//            var lst = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-//            {
-//                Date = DateTime.Now.AddDays(index),
-//                TemperatureC = Random.Shared.Next(-20, 55),
-//                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-//            })
-//                .Where(forecast => forecast.TemperatureC >= 10 && forecast.TemperatureC <= 20)
-//                .ToList();
-//            return lst;
-//            // var filteredList = lst.Where(forecast => forecast.TemperatureC >= 10 && forecast.TemperatureC <= 20);
-//            // var filteredList = new List<WeatherForecast>();
-//            // foreach (var forecast in filteredList)
-//            // {
-//            //     if (forecast.TemperatureC >= 10 && forecast.TemperatureC <= 20)
-//            //     {
-//            //         filteredList.Add(forecast);
-//            //     }
-//            // }
-//            // return filteredList;
-
-//        }
-
-
-//    }
-//}
+        //}
+        [HttpPost]
+        public WeatherForecast Createnew(WeatherForecast weatherForecast)
+        {
+            var ret = new WeatherForecast
+            {
+                Date = weatherForecast.Date,
+                TemperatureC = weatherForecast.TemperatureC,
+                TemperatureF = weatherForecast.TemperatureF,
+                Summary = weatherForecast.Summary
+            };
+            // return ret;
+            var db = new WeatherForecastContext();
+            db.WeatherForecasts.ToList();
+            db.WeatherForecasts.Add(ret);
+            return weatherForecast;
+        }
+    }
+}
 
 //using Microsoft.AspNetCore.Mvc;
 //using WebApplication1.Models;
@@ -88,48 +105,48 @@
 //    }
 //}
 
-using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using WebApplication1.Models;
 
-namespace WebApplication1.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private readonly WeatherForecastContext _context;
-        public WeatherForecastController(WeatherForecastContext context)
-        {
-            _context = context;
-        }
+//namespace WebApplication1.Controllers
+//{
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class WeatherForecastController : ControllerBase
+//    {
+//        private readonly WeatherForecastContext _context;
+//        public WeatherForecastController(WeatherForecastContext context)
+//        {
+//            _context = context;
+//        }
 
 
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            return Ok(_context.WeatherForecasts.ToList());
-        }
+//        [HttpGet]
+//        public IActionResult GetAll()
+//        {
+//            return Ok(_context.WeatherForecasts.ToList());
+//        }
 
-        [HttpPost]
-        public async Task<ActionResult<WeatherForecast>> PostWeatherForecast(WeatherForecast weatherForecast)
-        {
-            try
-            {
-                var ret = new WeatherForecast
-                {
-                    Date = weatherForecast.Date,
-                    TemperatureC = weatherForecast.TemperatureC,
-                    TemperatureF = weatherForecast.TemperatureF,
-                    Summary = weatherForecast.Summary
-                };
-                _context.WeatherForecasts.Add(ret);
-                _context.SaveChanges();
-                return Ok(ret);
-            }
-            catch
-            {
-                return BadRequest();
-            }
-        }
-    }
-}
+//        [HttpPost]
+//        public async Task<ActionResult<WeatherForecast>> PostWeatherForecast(WeatherForecast weatherForecast)
+//        {
+//            try
+//            {
+//                var ret = new WeatherForecast
+//                {
+//                    Date = weatherForecast.Date,
+//                    TemperatureC = weatherForecast.TemperatureC,
+//                    TemperatureF = weatherForecast.TemperatureF,
+//                    Summary = weatherForecast.Summary
+//                };
+//                _context.WeatherForecasts.Add(ret);
+//                _context.SaveChanges();
+//                return Ok(ret);
+//            }
+//            catch
+//            {
+//                return BadRequest();
+//            }
+//        }
+//    }
+//}
